fe992b14a540eb3e658e27cc5eaeb032921d3288
NaimurRahmannn <niamurreiman@gmail.com>
2025-12-19 01:20:51 +0600

    add propmpt and exit print

--- Prompt.c
+++ Prompt.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
+#include "shell.h"
 
 void print_prompt1(void)
-{   
-    struct symtab_entry_s *entry = get_symtab_entry("PS1");
-
-    if(entry && entry->val)
-    {
-        fprintf(stderr, "%s", entry->val);
-    }
-    else
-    {
-        fprintf(stderr, "[NIshh]> $ ");
-    }
+{
+    fprintf(stderr, "[mshX]:$ ");
 }
 
-void print_prompt2(void){
-    fprintf(stderr,"> ");
+void print_prompt2(void)
+{
+    fprintf(stderr, "> ");
 }
